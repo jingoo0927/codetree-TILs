9a9552a08a077b2fa30f90f75513ddd847fc7051
jingoo0927 <168266091+jingoo0927@users.noreply.github.com>
2024-07-09 16:30:01 +0900

    Title: 변수 출력하기 3, Time: 81ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240709/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 1;
+        int b = 2;
+        char c = 'C';
+
+        String sp = "->";
+
+        System.out.println(a + sp + b + sp + c);
+    }
+}
